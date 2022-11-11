409e0228995db5e46ecde906cd85397334b3ae18
akr <akr@akr.com>
2022-11-11 06:18:24 +0100

    splitting the input

--- main.rs
+++ main.rs
@@ -1,13 +1,30 @@
-use std::env;
 use std::fs;
 
 fn main() {
     // --snip--
     let file_path = "test_input";
-    println!("In file {}", file_path);
 
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
 
-    println!("With text:\n{contents}");
+    let split = contents.split("\n");
+    let mut first = true;
+    let mut prev:u8 = 0;
+    for s in split {
+        println!("number: {}",s);
+        let f: u8 = s.trim().parse().expect("Wanted a number");
+        if first
+        {
+            prev = f;
+            first = false;
+        }
+        else if f < prev
+        {
+            println!("s: decreased");
+        }
+        else if f > prev
+        {
+            println!("s: increased");
+        }
+    }
 }
